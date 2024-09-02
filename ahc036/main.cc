0cdbbad9ae8f4a7fb85467b53baa536665c770fb
Thomas Li <tli186@ucr.edu>
2024-09-02 03:31:10 -0600

    route the path

--- ahc036/main.cc
+++ ahc036/main.cc
@@ -211,8 +211,8 @@ class Solver {
 
   void ConnectNodes() {
     std::vector<int> nhubdist(N, N + 5);  // distance to nearest hub
-    std::vector<std::set<int>> nhub(N);   // nearest hub(s)
-    std::queue<std::array<int, 2>> q;     // <hub, node>
+    nhub.assign(N, {});
+    std::queue<std::array<int, 2>> q;  // <hub, node>
     for (int i = 0; i < H; ++i) q.push({i, hubs[i]});
     for (int depth = 0; !q.empty(); ++depth) {
       int qn = q.size();
@@ -271,15 +271,45 @@ class Solver {
       }
     }
     std::sort(hwo.rbegin(), hwo.rend());
+    std::vector<std::tuple<double, int, int>> nwo;  // node-to-hub weight order
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < H; ++j) {
+        if (nw[i][j]) nwo.push_back({nw[i][j], i, j});
+      }
+    }
+    std::sort(nwo.rbegin(), nwo.rend());
 
     // for (auto [w, u, v] : hwo) std::cout << u << "->" << v << " " << w <<
     // "\n";
     std::vector<std::deque<int>> paths;
-    std::vector<int> usage(N, -1);  // -1 if unassigned, -2 if assigned
-                                    // >=0 if endpoint of a path
-                                    // 2i if the front of the ith path
-                                    // (2i+1) if the back of the ith path
-    for (auto [w, s, t] : hwo) {    // IN HUB SPACE
+
+    // -1 if unassigned, -2 if assigned
+    // >=0 if endpoint of a path
+    // 2i if the front of the ith path
+    // (2i+1) if the back of the ith path
+    std::vector<int> usage(N, -1);
+
+    auto AppendToPath = [&](int ref, int u) -> void {
+      int e = ref;
+      if (!(e & 1)) paths[e / 2].push_front(u);
+      if (e & 1) paths[e / 2].push_back(u);
+    };
+    auto PopFromPath = [&](int ref) -> int {
+      int e = ref;
+      int o = -1;
+      auto &p = paths[e / 2];
+      if (!(e & 1)) {
+        o = p.front();
+        p.pop_front();
+      } else {
+        o = p.back();
+        p.pop_back();
+      }
+      return o;
+    };
+    std::vector<std::vector<int>> uv_paths;
+
+    for (auto [w, s, t] : hwo) {  // IN HUB SPACE
       // find path from s to t (in hub space)
       std::vector<int> vis(H), st_path, uv_path;
       std::priority_queue<std::pair<int, std::vector<int>>> pq;
@@ -309,6 +339,8 @@ class Solver {
         for (auto u : h_path[s][t]) uv_path.push_back(u);
       }
 
+      uv_paths.push_back(uv_path);
+
       /* printing the hpath and path
       // std::cout << "h" << s << "->h" << t << " w=" << w << " | ";
       // for (auto x : st_path) std::cout << x << " ";
@@ -322,21 +354,64 @@ class Solver {
       // for (auto x : uv_path) std::cout << x << " ";
       // std::cout << "\n\n";
       */
+    }
 
-      auto AppendToPath = [&](int ref, int u) -> void {
-        int e = ref;
-        if (!(e & 1)) paths[e / 2].push_front(u);
-        if (e & 1) paths[e / 2].push_back(u);
-      };
+    nh_path.assign(N, std::vector<std::vector<int>>(H));
+    for (auto [w, s, h] : nwo) {  // node to hub space
+      // std::cout << s << "->h" << h << " w=" << w << "\n";
+      std::vector<int> vis(N);
+      std::queue<std::vector<int>> q;
+      q.push({s});
+      while (!q.empty()) {
+        auto path = q.front();
+        int u = path.back();
+        q.pop();
+        if (vis[u]) continue;
+        vis[u] = 1;
+        if (u == hubs[h]) {
+          nh_path[s][h] = path;
+          uv_paths.push_back(std::move(path));
+          break;
+        }
+        for (auto v : adj[u]) {
+          if (vis[v]) continue;
+          auto path2 = path;
+          path2.push_back(v);
+          q.push(std::move(path2));
+        }
+      }
+    }
 
+    int Ai = 0;                  // Ai'th assignemnt in A
+    int N_used = 0;              // how many nodes are used in paths
+    std::set<int> unfulfilled;   // nodes that havent been written yet
+    std::vector<int> n_used(N);  // node gets used in a path
+    for (const auto &uv_path : uv_paths) {
+      for (auto u : uv_path) {
+        if (n_used[u]) continue;
+        n_used[u] = 1;
+        ++N_used;
+        unfulfilled.insert(u);
+      }
+    }
+    for (auto u : targets) unfulfilled.insert(u);
+    for (int i = 0; i < unfulfilled.size(); ++i) unfulfilled.insert(i);
+
+    int extras = LA - N;
+    for (const auto &uv_path : uv_paths) {
       for (int i = 1; i < uv_path.size(); ++i) {
         int u = uv_path[i - 1], v = uv_path[i];
-        if (usage[v] == -1) std::swap(u, v);  // ensure if there is a -1, its u first
-        // if usage[u] == -2 (assigned) -- do nothing
-        if (usage[u] == -1) {  // one unassigned
-          // if usage[v] == -2 (assigned) -- do nothing
-          // in future -- you'll take into account extra A space
-          if (usage[v] == -1) {  // both unassigned
+        if (usage[v] == -1)
+          std::swap(u, v);       // ensure if there is a -1, its u first
+        if (usage[u] == -1) {    // one unassigned
+          if (usage[v] == -2) {  // will need extra A space
+            if (extras >= 1) {
+              --extras;
+              usage[u] = usage[v] = 2 * paths.size();
+              ++usage[v];
+              paths.push_back({u, v});
+            }
+          } else if (usage[v] == -1) {  // both unassigned
             usage[u] = usage[v] = 2 * paths.size();
             ++usage[v];
             paths.push_back({u, v});
@@ -351,22 +426,144 @@ class Solver {
             // whoa combine the paths
             int eu = usage[u];
             int ev = usage[v];
-            if (eu == ev) continue; // uh you can skip this
+            if (eu == ev) continue;        // uh you can skip this
+            if ((eu ^ ev) == 1) continue;  // you can also skip this
             usage[u] = usage[v] = -2;
-            AppendToPath(eu, v);
-            // AppendToPath(ev, u);
+            // AppendToPath(eu, v);
+            // std::cout << "moving" << ev << "->" << eu << std::endl;
+            // move the entirety of ev over
+            while (!paths[ev / 2].empty()) AppendToPath(eu, PopFromPath(ev));
+            // replace ev's complement
+            for (auto &x : usage) {
+              if (x == (ev ^ 1)) x = eu;  // now you're the other end
+            }
+          } else if (usage[v] == -2) {
+            if (extras >= 1) {
+              --extras;
+              int e = usage[u];
+              usage[v] = e;
+              usage[u] = -2;
+              AppendToPath(e, v);
+            }
           }
-          // else usage[v] == -2 ;; assigned
+        } else {  // usage[u] == -2 ;; aka both are somehow assigned
+          if (extras >= 2) {
+            extras -= 2;
+            usage[u] = usage[v] = 2 * paths.size();
+            ++usage[v];
+            paths.push_back({u, v});
+          }
         }
       }
     }
-    std::cout << "var paths=[\n";
-    for (auto &path : paths) {
-      std::cout << "[";
-      for (auto x : path) std::cout << x << ",";
-      std::cout << "],";
+
+    // std::cout << "var paths=[\n";
+    // for (auto &path : paths) {
+    //   if (path.empty()) continue;
+    //   std::cout << "[";
+    //   for (auto x : path) std::cout << x << ",";
+    //   std::cout << "],";
+    // }
+    // std::cout << "\n];\n";
+    // int tot = 0;
+    // for (auto &path : paths) tot += path.size();
+    // std::cout << tot << "\n";
+
+    for (const auto &path : paths) {
+      if (path.empty()) continue;
+      for (auto u : path) {
+        if (Ai >= A.size()) break;
+        A[Ai++] = u;
+        unfulfilled.erase(u);
+        // std::cout << u << " ";
+      }
+      // std::cout << "\n";
     }
-    std::cout << "\n];\n";
+    // std::cerr << (LA - Ai) << " free, need " << unfulfilled.size() << std::endl;
+    for (auto u : unfulfilled) {
+      A[Ai++] = u;
+      // std::cerr << u << " " << std::flush;
+    }
+    for (int i = 0; i < N && Ai < A.size(); ++i) {
+      if (!n_used[i]) A[Ai++] = i;
+    }
+  }
+
+  void Solve(std::ostream &os) {
+    for (auto x : A) os << x << " ";
+    os << std::endl;
+
+    std::vector<std::vector<int>> ap(N);  // where the node exists in A
+    for (int i = 0; i < LA; ++i) ap[A[i]].push_back(i);
+
+    // going through the travel targets
+    for (int i = 0; i < T; ++i) {
+      int s = i ? targets[i - 1] : 0;  // source
+      int t = targets[i];              // terminal
+      // std::cerr << s << "->" << t << std::endl;
+      std::vector<int> path;
+      for (int hs : nhub[s]) {
+        for (int ht : nhub[t]) {
+          // std::cerr << hs << "-h->" << ht << std::endl;
+          std::vector<int> p;  // candidate path
+          for (int u : nh_path[s][hs]) p.push_back(u);
+          p.pop_back();
+          for (int u : h_path[hs][ht]) p.push_back(u);
+          p.pop_back();
+          auto rev = nh_path[t][ht];
+          std::reverse(rev.begin(), rev.end());
+          for (int u : rev) p.push_back(u);
+          // std::cerr << "score=" << p.size() << std::endl;
+          if (path.empty() || p.size() < path.size()) path = p;
+        }
+      }
+      // for (auto u : path) os << u << " ";
+      // std::cout << std::endl;
+
+      // route the path now
+      for (int i = 1; i < path.size(); ++i) {
+        auto u = path[i];  // the next node
+        if (!InB(u)) {
+          int score = 0;
+          std::array<int, 3> s_op_param;
+
+          for (int j : ap.at(u)) {
+            std::vector<int> ai_candidates = {
+                std::min(j, LA - LB),
+                std::max(0, j - LB + 1),
+            };
+            for (int ai : ai_candidates) {
+              std::array<int, 3> sop = {LB, ai, 0};
+              int sop_score = 0;
+              std::set<int> B2;
+              for (int k = 0; k < LB; ++k) B2.insert(A[sop[1] + k]);
+              for (int k = i; k < path.size(); ++k) {
+                if (B2.count(path[k]))
+                  ++sop_score;
+                else
+                  break;
+              }
+              if (sop_score > score) {
+                score = sop_score;
+                s_op_param = sop;
+              }
+            }
+          }
+          // std::cout << "# score=" << score << std::endl;
+
+          os << "s ";
+          for (auto x : s_op_param) os << x << " ";
+          os << "\n";
+
+          auto [l, ai, bi] = s_op_param;
+          for (int i = 0; i < l; ++i) B[bi + i] = A[ai + i];
+        }
+        os << "m " << u << " ";
+        std::cout << "\n";
+        // std::cout << "\n# ";
+        // for (auto x : B) std::cout << x << " ";
+      }
+    }
   }
 
   /**
@@ -399,12 +596,22 @@ class Solver {
   std::vector<int> A, B;              // active nodes (slow/fast memory)
   std::vector<int> targets;           // paths that must be taken
   std::vector<std::array<int, 2>> coords;
+  std::vector<std::vector<std::vector<int>>> nh_path;  // path node to hub
+  std::vector<std::set<int>> nhub;  // nearest hub(s) for a node
 
   int H;
   std::vector<int> hubs;                               // which nodes are hubs
   std::vector<std::vector<std::array<int, 2>>> h_adj;  // adj-list for hubs
   std::vector<std::vector<std::vector<int>>> h_path;   // APSP over hub paths
   std::vector<std::vector<int>> h_dist;                // APSP over hubs
+
+  // returns true if u is in B
+  bool InB(int u) {
+    for (auto x : B) {
+      if (x == u) return true;
+    }
+    return false;
+  }
 };
 
 int32_t main() {
@@ -414,6 +621,7 @@ int32_t main() {
   sol.AssignHubs();
   sol.ConnectHubs();
   sol.ConnectNodes();
+  sol.Solve(std::cout);
   // sol.Render(std::cout);
   return 0;
 }
